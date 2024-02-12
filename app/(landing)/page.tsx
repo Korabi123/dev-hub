59e8a1cba347607273422015396f48ba37243929
Korab Imeri <korabimeri91@gmail.com>
2024-02-12 12:30:15 +0100

    style:added links to legal terms in footer

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,6 +5,8 @@ import { ButtonFlickeringLight } from "@/components/special-button";
 import { ChevronsLeftRight, Globe } from "lucide-react";
 import { CardSpotlight } from "@/components/card-spotlight";
 import { currentUser } from "@/lib/auth";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   const user = await currentUser();
@@ -72,11 +74,35 @@ export default async function Home() {
           </div>
         </section>
       </main>
-      <footer className="bg-white border-t">
-        <div className="mx-auto py-10">
-          <p className="text-center text-xs text-black">
-            &copy; 2023 DevHub, All rights reserved.
-          </p>
+      <footer className="w-full border-t justify-center">
+        <div className="container flex flex-col py-4 gap-4 px-4 items-center justify-center text-center md:flex-row md:gap-6 md:px-6 lg:py-8 xl:max-w-6xl xl:gap-8 xl:justify-between">
+          <div className="flex flex-col gap-2 text-sm md:items-center md:gap-4 md:order-1 md:text-base lg:order-0">
+            <Link
+              className="font-semibold underline hover:none"
+              href="/terms-of-service"
+            >
+              Terms of Service
+            </Link>
+            <Link className="underline hover:none" href="/privacy-policy">
+              Privacy Policy
+            </Link>
+          </div>
+          <div className="flex items-center justify-center space-x-4 md:order-0">
+            <Link className="text-2xl font-bold sr-only" href="/">
+              DevHub
+            </Link>
+            <Image
+              src="/logo-base-1200x1200.png"
+              alt="DevHub Logo"
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className="flex flex-col gap-2 text-sm md:items-center md:gap-4 md:order-1 md:text-base lg:order-0">
+            <p className="text-gray-500 dark:text-gray-400">
+              © 2024 DevHub Inc.
+            </p>
+          </div>
         </div>
       </footer>
     </>
