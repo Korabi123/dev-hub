a58f137a37af8f6c1fdce6355745556b8cc6b4a2
Korab Imeri <korabimeri91@gmail.com>
2024-02-12 12:16:54 +0100

    Revert "fix:images in dark mode fix"
    This reverts commit 031c2f8c68ba763b5a6995d3b1d180e78c0e2704.

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,10 +11,7 @@ export const Footer = () => {
     <footer className="w-full border-t justify-center">
       <div className="container flex flex-col py-4 gap-4 px-4 items-center justify-center text-center md:flex-row md:gap-6 md:px-6 lg:py-8 xl:max-w-6xl xl:gap-8 xl:justify-between">
         <div className="flex flex-col gap-2 text-sm md:items-center md:gap-4 md:order-1 md:text-base lg:order-0">
-          <Link
-            className="font-semibold underline hover:none"
-            href="/terms-of-service"
-          >
+          <Link className="font-semibold underline hover:none" href="/terms-of-service">
             Terms of Service
           </Link>
           <Link className="underline hover:none" href="/privacy-policy">
@@ -25,20 +22,11 @@ export const Footer = () => {
           <Link className="text-2xl font-bold sr-only" href="/">
             DevHub
           </Link>
-          <Image
-            src="/logo-base-1200x1200.png"
-            alt="DevHub Logo"
-            width={50}
-            height={50}
-            className="block dark:hidden"
-          />
-          <Image
-            src="/logo-white-1200x1200.png"
-            alt="DevHub Logo"
-            width={50}
-            height={50}
-            className="dark:block hidden"
-          />
+          {resolvedTheme === "light" ? (
+            <Image src="/logo-base-1200x1200.png" alt="DevHub Logo" width={50} height={50} />
+          ): (
+            <Image src="/logo-white-1200x1200.png" alt="DevHub Logo" width={50} height={50} />
+          )}
         </div>
         <div className="flex flex-col gap-2 text-sm md:items-center md:gap-4 md:order-1 md:text-base lg:order-0">
           <p className="text-gray-500 dark:text-gray-400">© 2024 DevHub Inc.</p>
@@ -46,4 +34,4 @@ export const Footer = () => {
       </div>
     </footer>
   );
-};
+}
